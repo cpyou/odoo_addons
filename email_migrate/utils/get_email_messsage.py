b6ad40f3c0f4bf6597f050ff2b4bda80f5a5aceb
puyu.chen <745690939@qq.com>
2015-04-09 17:42:37 +0800

    修改邮件迁移

--- email_migrate/utils/get_email_messsage.py
+++ email_migrate/utils/get_email_messsage.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
-import urllib3
+import requests
 
 server = 'http://mail.163.com/'
 
-http = urllib3.PoolManager()
+r = requests.get(server, auth=('cpyoulv@163.com', 'haoyunqiwyyx'))
+r.encoding = 'utf-8'
+f = open('./test.html', 'w')
+f.write(r.text)
+# f.seek(0)
+f.close()
 
-http.request('post', server, {'username': 'cpyoulv@163.com', 'password': 'haoyunqiwyyx'})
-
-url = 'http://mail.163.com/js6/main.jsp?sid=NCSQIbwSwkFhBJvalESSJvlxFrADLQMw&df=unknow#module=mbox.ListModule%7C%7B%22fid%22%3A1%2C%22order%22%3A%22date%22%2C%22desc%22%3Atrue%7D'
-r = http.request('GET', url)
-
-print r.status, r.data
+print r.status_code
