ade396d997e594af3fc2a46a583f154ec1b1f8fa
puyu.chen <745690939@qq.com>
2015-04-09 15:32:22 +0800

    修改安装邮件迁移模块报错

--- email_migrate/email_migrate.py
+++ email_migrate/email_migrate.py
@@ -6,7 +6,7 @@ from openerp.osv import orm
 
 class MigrateUSer(orm.Model):
     _name = 'migrate.user'
-    _description = '邮件迁移'
+    _description = 'migrate user'
     _rec_name = 'old_email_address'
 
     email_server = fields.Char('邮箱服务器')
@@ -15,14 +15,14 @@ class MigrateUSer(orm.Model):
     # need_handle_sum = fields.Float('')
     # handled_sum = fields.Float('')
     handling_server = fields.Char('当前A处理服务器')
-    messages = fields.One2many('email.message', )
+    messages = fields.One2many('email.message', 'user', '邮件')
 
 MigrateUSer()
 
 
 class EmailMessage(orm.Model):
     _name = 'email.message'
-    _description = '邮件'
+    _description = 'email message'
 
     message_id = fields.Integer('消息id')
     user = fields.Many2one('migrate.user', '用户')
@@ -36,7 +36,7 @@ EmailMessage()
 
 class MessageHandleHistory(orm.Model):
     _name = 'message.handle.history'
-    _description = '消息处理历史'
+    _description = 'message handle history'
 
     message_id = fields.Many2one('email.message', '邮件id')
     handle_server = fields.Many2one('handle.server', '处理服务器')
@@ -48,7 +48,7 @@ MessageHandleHistory()
 
 class HandleServer(orm.Model):
     _name = 'handle.server'
-    _description = '处理服务器'
+    _description = 'handle server'
 
     server_ip = fields.Char('服务器ip')
     work_state = fields.Selection([('wait_work', '等待工作'),
@@ -58,8 +58,8 @@ class HandleServer(orm.Model):
                                               'handle_server', '以处理处理消息历史')
 
     handling_email_address = fields.Many2one('migrate.user', '处理中的用户')
-    handled_email_address = fields.Many2many('handle.serve.migrate.user.rel',
-                                             'handle_server', 'email_address', '以处理的用户')
+    handled_email_address = fields.Many2many('migrate.user', 'handle_server_migrate_user_rel',
+                                             'handle_server_id', 'email_address_id', '已处理的用户')
 
 
 HandleServer()
