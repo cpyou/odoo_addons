51a8cddbc9bd73532c96b902eb6ed46b94463f52
cpyou <745690939@qq.com>
2016-03-01 16:03:13 +0800

    报表测试设计

--- report_test/report/report_test.py
+++ report_test/report/report_test.py
@@ -15,11 +15,22 @@ class ReportTest(report_int):
         # registry['stock.warehouse'].search(cr, uid, [])[0]
         fp = StringIO()
         wbk = xlsxwriter.Workbook(fp, {'in_memory': True})
-        sheet = wbk.add_worksheet('cpy')
-        sheet.write(0, 0, '报表测试')
+        sheet = wbk.add_worksheet('报表测试')
+        self.write_header(sheet)
+        self.write_body(sheet)
+        self.write_footer(sheet)
         wbk.close()
         fp.seek(0)
         pdf = fp.read()
         return pdf, 'xlsx'
+
+    def write_header(self, sheet):
+        sheet.write(0, 0, 'header')
+
+    def write_body(self, sheet):
+        sheet.write(0, 1, 'body')
+
+    def write_footer(self, sheet):
+        sheet.write(0, 2, 'footer')
 
 ReportTest('report.report.test')
